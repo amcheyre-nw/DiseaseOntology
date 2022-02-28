8c2ff338a2b3191bff0b17ff0238c74b4c424fc7
james-wilkinson-NW <91038008+james-wilkinson-NW@users.noreply.github.com>
2022-02-28 15:57:33 -0600

    cleaning repo

--- ULMS_api.py
+++ ULMS_api.py
@@ -50,10 +50,9 @@ class API:
         url = (f'{self.BASE_URL}/semantic-network/{self._version}/TUI/{tui}')
         return self._get(url=url)
 
-    def get_ui(self, cui):
+    def get_ui(self, ui):
         self._auth = Auth(api_key=api_key)  # need to call this multiple times? painful
-        url = f'{self.BASE_URL}/content/{self._version}/source/MSH/{ui}'
-        return self._get(url=url)
+        url = f'{self.BASE_URL}/content/{self._version}/source/MSH/{ui}' return self._get(url=url)
 
     def get_children(self, ui):
         self._auth = Auth(api_key=api_key) # need to call this multiple times? painful
@@ -68,21 +67,33 @@ class API:
 
 
 def build_tree(rootUI, _API):
-    tree = []
+    tree = ['class', 'superclass']
+    leafs = []
     stack = [rootUI]
     while len(stack) > 0:
         ui = stack.pop(0)
-        print(ui)
         req = _API.get_ui(ui)
         child_req = _API.get_children(ui)
         children = [x for x in child_req['result']]
-        tree = tree + [[req['result']['name'], child['name']] for child in children]
+        #tree = tree + [[req['result']['name'], child['name']] for child in children]
         stack = [child['ui'] for child in children if child['children'] != 'NONE'] + stack # add children to stack if they also have children
-    return tree
+
+        for child in children: # add the spo statements
+            childname = child['name']
+            parentname = req['result']['name']
+            tree.append([childname, parentname])
+
+            if child['children'] == "NONE":
+                leafs.append([childname, child['ui']])
+    return tree, leafs
 
 
-api_key = '66bc8361-7450-4750-861c-52ed6ae1dd18'
-api = API(api_key=api_key)
+if __name__ == '__main__':
+    api_key = '66bc8361-7450-4750-861c-52ed6ae1dd18'
+    api = API(api_key=api_key)
 
-ui = 'U000014'
-tree = build_tree(ui, api)
+    ui = 'D001523'
+    tree, leafs = build_tree(ui, api)
+
+    tree.to_csv('disease_classes.csv')
+    leafs.to_csv('leafs.csv')
